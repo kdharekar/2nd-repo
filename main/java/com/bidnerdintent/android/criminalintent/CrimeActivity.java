8fef794be41932ab0af7c043960c2d0be2fdf180
KD <kdharekar.iitd@gmail.com>
2020-07-11 10:25:45 +0530

    Criminal intent after adding Criminal list fragment i.e chapter 8 (it is not working)

--- main/java/com/bidnerdintent/android/criminalintent/CrimeActivity.java
+++ main/java/com/bidnerdintent/android/criminalintent/CrimeActivity.java
@@ -1,27 +1,13 @@
 package com.bidnerdintent.android.criminalintent;
 
-import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        Fragment fragment = new CrimeFragment();
-        return fragment ;
+
+        return new CrimeFragment() ;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if(fragment==null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
-        }
     }
-}
